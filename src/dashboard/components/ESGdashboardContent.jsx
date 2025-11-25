0c82614fde9f325e12662511ad206c613b7d2ec0
Yancong <xieyancong@gmail.com>
2025-11-25 12:25:54 +1100

    fix Scope 3

--- src/dashboard/components/ESGdashboardContent.jsx
+++ src/dashboard/components/ESGdashboardContent.jsx
@@ -7,7 +7,7 @@ import autoTable from 'jspdf-autotable';
 import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded';
 
 const metricsOptions = ['Metric 1', 'Metric 2', 'Metric 3'];
-const esgOptions = ['GRI', 'AASB S2', 'AASB Sscope 3'];
+const esgOptions = ['GRI', 'AASB S2', 'AASB Scope 3'];
 
 // Data processing function
 const processData = (data) => {
